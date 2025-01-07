550b58c5c919a1397680a58c6710d8665b8bb7d4
hbarogh <hxb1538@mavs.uta.edu>
2025-01-06 20:29:13 -0600

    Added changes and improvements to the products page.

--- front-end/src/pages/ProductPage.vue
+++ front-end/src/pages/ProductPage.vue
@@ -1,9 +1,27 @@
 <template>
-  <h1>Products Page</h1>
+  <h1>Products</h1>
+  <div class="grid-wrap">
+    <div 
+      class="product-item"
+      v-for="product in prodcuts"
+      :key="product.id"
+    >
+      <img :src="product.imageName"/>
+      <h3 class="product-name">{{ product.name }}</h3>
+      <p class="product-price">{{ product.price }}</p>
+      <button>View Details</button>
+    </div>
+  </div>
 </template>
 
 <script>
+  import {prodcuts} from '../temp-data';
   export default {
     name: "ProductsPage",
+    data(){
+      return{
+        prodcuts,
+      }
+    }
   }
 </script>
