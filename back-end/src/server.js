082d85bb5f8160a88ed00c721a4edd29c7ea3d24
hbarogh <hxb1538@mavs.uta.edu>
2025-01-07 18:22:51 -0600

    Finished making the code be able to use the data that's in the mongo db database, and removed the temporary data file.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,55 +1,65 @@
 //entry point for the server for the backend code 
 import express from 'express';
 import {MongoClient} from 'mongodb';
-import { cartItems as cartItemsRaw, products as productsRaw} from './temp-data';
 
-let cartItems = cartItemsRaw;
-let products = productsRaw;
-const url = `mongodb+srv://fsv-server:_2mrsGW2xJp!JuF@cluster0.weuhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const client = new MongoClient(url);
-const app = express();
-app.use(express.json());
-app.get('/hello', async (req, res) => {
+
+async function start(){
+  const url = `mongodb+srv://fsv-server:_2mrsGW2xJp!JuF@cluster0.weuhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  const client = new MongoClient(url);
   await client.connect();
   const db = client.db('fsv-db');
-  const products = await db.collection('products').find({}).toArray();
-  res.send(products);
-});
+  
+  const app = express();
+  app.use(express.json());
+  
+  
+  app.get('/products', async (req,res) => {
+    const products = await db.collection('products').find({}).toArray();
+    res.send(products);
+  });
+  
+  async function populateCardIds(ids) {
+    return Promise.all(ids.map(id => db.collection('products').findOne({id})));
+  }
+  
+  app.get('/users/:userId/cart', async (req,res) => {
+    const user = await db.collection('users').findOne({id: req.params.userId});
+    const populatedCart = await populateCardIds(user.cartItems);
+    res.json(populatedCart);
+  });
+  
+  app.get('/products/:productId', async (req,res) => {
+    const productId = req.params.productId;
+    const product = await db.collection('products').findOne({id: productId});
+    res.json(product);
+  });
+  
+  app.post('/users/:userId/cart', async (req, res) => {
+    const userId = req.params.userId;
+    const productId = req.body.id;
+    await db.collection('users').updateOne({id: userId}, { //this is an example of an update query 
+      $addToSet: {cartItems: productId} //want to push productId onto the user's cartItems property 
+    });
+    const user = await db.collection('users').findOne({id: req.params.userId});
+    const populatedCart = await populateCardIds(user.cartItems);
+    res.json(populatedCart);
+  });
+  
+  app.delete('/users/:userId/cart/:productId', async (req,res) => {
+    const userId = req.params.userId;
+    const productId = req.params.productId;
 
-app.get('/products', (req,res) => {
-  res.json(products); //we do .json instead of .get because we are sending json data
-});
-
-function populateCardIds(ids) {
-  return ids.map(id => products.find(product => product.id === id));
-
+    await db.collection('users').updateOne( {id: userId},{
+      $pull: {cartItems: productId} //we are removing an item from the user's cart here
+    });
+    const user = await db.collection('users').findOne({id: req.params.userId});
+    const populatedCart = await populateCardIds(user.cartItems);
+    res.json(populatedCart);
+  });
+  
+  app.listen(8000, () => {
+    console.log('Server is listening on port 8000')
+  });
 }
 
-app.get('/cart', (req,res) => {
-  const populatedCart = populateCardIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.get('/products/:productId', (req,res) => {
-  const productId = req.params.productId;
-  const product = products.find(product => product.id === productId);
-  res.json(product);
-});
-
-app.post('/cart', (req, res) => {
-  const productId = req.body.id;
-  cartItems.push(productId);
-  const populatedCart = populateCardIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.delete('/cart/:productId', (req,res) => {
-  const productId = req.params.productId;
-  cartItems = cartItems.filter(id => id !== productId);
-  const populatedCart = populateCardIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.listen(8000, () => {
-  console.log('Server is listening on port 8000')
-});
+start();
