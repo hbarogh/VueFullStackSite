059a375ea21f1f84a80f25f39e3ccc77b5fb1d52
hbarogh <hxb1538@mavs.uta.edu>
2025-01-08 12:39:54 -0600

    Added firebase authentication

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -7,6 +7,24 @@ import ProductsPage from './pages/ProductsPage.vue'
 import ProductDetailPage from './pages/ProductDetailPage.vue'
 import NotFoundPage from './pages/NotFoundPage.vue';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDm75lJRCU9WMbyojAMkZXWnUjXRS7CnII",
+  authDomain: "vue-site-cdb9c.firebaseapp.com",
+  projectId: "vue-site-cdb9c",
+  storageBucket: "vue-site-cdb9c.firebasestorage.app",
+  messagingSenderId: "968034649563",
+  appId: "1:968034649563:web:be9db8db8f8f369e6b53fe"
+};
+
+
+initializeApp(firebaseConfig);
+
 createApp(App)
 .use(VueRouter.createRouter({
   history: VueRouter.createWebHistory(process.env.BASE_URL),
